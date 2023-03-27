57fd796e2226096b57c44ef5384b5aeab37403ce
Artem Belevich <artemb@gmail.com>
2023-03-26 21:30:00 -0700

    Cosmetic cleanups

--- dsl2sr.py
+++ dsl2sr.py
@@ -5,31 +5,31 @@ import zipfile
 import numpy as np
 
 warnings.simplefilter("ignore")
- 
+
 # Parse command line arguments
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument('-i', '--input', type=str)
-
-# Add an optional argument for the output file,
-# open in 'write' mode and and specify encoding
 parser.add_argument('-o', '--output', type=str)
 args = parser.parse_args()
 
 
 def merge_bitstreams(data):
+    """Merges together per-channel bitstreams into packed unitsize samples used by sigrok"""
     num_streams = len(data)
-    bits = [np.unpackbits(np.frombuffer(d, dtype=np.uint8), bitorder='little') for d in data]
-    return np.packbits(np.stack(bits, axis = 1), axis = -1, bitorder='little').tobytes()
-
-
+    # Convert input data into arrays of individual bits
+    bits = [np.unpackbits(np.frombuffer(d, dtype=np.uint8),
+                          bitorder='little') for d in data]
+    # Then stack them all and pack into all-channels units
+    return np.packbits(np.stack(bits, axis=1), axis=-1, bitorder='little').tobytes()
 
 
 def copy_data(num_probes, num_blocks, dsl, sr):
+    # Convert individual per-channel DSL data blocks into sigrok's combined data slices.
     for b in range(num_blocks):
         dsl_data = [dsl.read("L-%d/%d" % (p, b)) for p in range(num_probes)]
-        sr_data = merge_bitstreams(dsl_data)
         with sr.open("logic-1-%d" % (b+1), "w") as data:
-            data.write(sr_data)
+            data.write(merge_bitstreams(dsl_data))
+
 
 def copy_metadata(dsl, sr):
     num_probes = 0
@@ -42,38 +42,43 @@ total analog=0
 capturefile=logic-1
 """
     with dsl.open("header", mode="r") as dslm:
-            for line in dslm:
-                line = line.decode('utf-8').strip()
-                kv = [ x.strip() for x in line.split('=')]
-                if len(kv) !=2:
-                    continue
-                k, v = kv
-                if k == "total probes":
-                    num_probes = int(v)
-                elif k == "total blocks":
-                    num_blocks = int(v)
-                if k.startswith("probe"):
-                    p = int(k[5:])+1
-                    sr_metadata += ("probe%d=%s\n" % (p, v))
-                if k in ["samplerate", "total probes"]:
-                    sr_metadata += ("%s=%s\n" % (k, v))
+        for line in dslm:
+            line = line.decode('utf-8').strip()
+            kv = [x.strip() for x in line.split('=')]
+            if len(kv) != 2:
+                continue
+            k, v = kv
+            if k == "total probes":
+                num_probes = int(v)
+            elif k == "total blocks":
+                num_blocks = int(v)
+            if k.startswith("probe"):
+                # DSL uses 0-based probe numbering, sigrok wants 1-based.
+                p = int(k[5:])+1
+                sr_metadata += ("probe%d=%s\n" % (p, v))
+            if k in ["samplerate", "total probes"]:
+                sr_metadata += ("%s=%s\n" % (k, v))
+    # figure out how many bytes we need to pack all channels.
     unitsize = (num_probes + 7) // 8
     sr_metadata += "unitsize=%d" % unitsize
     with sr.open("metadata", "w") as srm:
         srm.write(sr_metadata.encode('utf-8'))
 
-    print("Found %d probes/%d blocks" %(num_probes, num_blocks))
+    print("Found %d probes/%d blocks of data" % (num_probes, num_blocks))
     return num_probes, num_blocks
+
 
 def convert(dsl, sr):
     num_probes, num_blocks = copy_metadata(dsl, sr)
     copy_data(num_probes, num_blocks, dsl, sr)
-    
+
+
 def main():
     with zipfile.ZipFile(args.input, mode="r") as dsl:
         with zipfile.ZipFile(args.output, mode="w", compression=zipfile.ZIP_DEFLATED) as sr:
             sr.writestr("version", "2")
             convert(dsl, sr)
+
 
 if __name__ == "__main__":
     main()
